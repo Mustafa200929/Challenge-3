6a92fc0e7be1ab9bab233748d8b268a706559243
Mustafa <mus383728@gmail.com>
2025-11-15 09:23:47 +0800

    Initial Commit

---
+++ Challenge 3/Challenge_3App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge_3App.swift
+//  Challenge 3
+//
+//  Created by Mustafa Topiwala on 15/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Challenge_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
