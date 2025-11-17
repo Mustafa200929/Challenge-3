3f63dfbd0dd8c2d0ba68e5e1aed613ea0124fa76
adhavan950 <adhavan_senthil_kumar@students.edu.sg>
2025-11-17 14:30:28 +0800

    sdfjkBF

--- Challenge 3/Views/CameraView.swift
+++ Challenge 3/Views/CameraView.swift
@@ -5,14 +5,55 @@
 //  Created by Adhavan senthil kumar on 17/11/25.
 //
 
+//  CameraView.swift
+//  Challenge 2
+//
+//  Created by Mustafa Topiwala on 28/8/25.
+//
+import Foundation
 import SwiftUI
+import UIKit
 
-struct CameraView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct CameraView: UIViewControllerRepresentable{
+    @Binding var image: UIImage?
+    @Environment(\.presentationMode) var presentationMode
+    
+    func makeUIViewController(context: Context)-> UIImagePickerController{
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        picker.sourceType = .camera
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+        //no updates needed
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+        var parent: CameraView
+        
+        init(_ parent: CameraView) {
+            self.parent = parent
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let image = info[.originalImage] as? UIImage{
+                parent.image = image
+            }
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+        
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            parent.presentationMode.wrappedValue.dismiss()
+        }
     }
 }
 
 #Preview {
-    CameraView()
+    @State var Image: UIImage? = nil
+    CameraView(image: $Image)
 }
--- Challenge 3/Views/Plantsheet2.swift
+++ Challenge 3/Views/Plantsheet2.swift
@@ -6,75 +6,96 @@
 //
 import SwiftUI
 
-struct PlantSheet: View {
+struct Plantsheet2: View {
     @Binding var selectedDetent: PresentationDetent
-    @StateObject var plantVM = PlantModelView()
+    @StateObject var plantVM = PlantViewModel()
     
     var body: some View {
-        VStack{
-            HStack{
-                //Small bar
-                Image(systemName: plantVM.plants[0].plantIconName)
+        NavigationStack {
+            VStack {
+                HStack {
+                    Image(systemName: plantVM.plants[0].plantIconName)
+                        .padding()
+                        .glassEffect(.regular.tint(.teal.opacity(0.3)))
+
+                    VStack {
+                        Text(plantVM.plants[0].plantName)
+                            .font(.system(size: 24, weight: .bold, design: .rounded))
+                        Text(plantVM.plants[0].plantType)
+                    }
+                    .frame(alignment: .topLeading)
                     .padding()
-                    .glassEffect(.regular.tint(.teal.opacity(0.3)))
-                VStack{
-                    Text(plantVM.plants[0].plantName)
-                        .font(.system(size: 24, weight: .bold, design: .rounded))
-                    Text(plantVM.plants[0].plantType)
+
+                    Spacer()
+
+                    if plantVM.plants[0].plantAge == 1 {
+                        Text("\(plantVM.plants[0].plantAge) day old")
+                            .fontWeight(.semibold)
+                    } else {
+                        Text("\(plantVM.plants[0].plantAge) days old")
+                            .fontWeight(.semibold)
+                    }
                 }
-                .frame(alignment: .topLeading)
                 .padding()
+
+                if selectedDetent == .medium || selectedDetent == .large {
+                    VStack {
+                        VStack {
+                            Text("Should germinate in \(plantVM.plants[0].daysToGerminate) days")
+                                .padding(.horizontal)
+                                .padding(.top)
+                                .fontWeight(.medium)
+                            Text(plantVM.plants[0].message)
+                                .padding(.bottom)
+                        }
+                        .background(.black.opacity(0.12))
+                        .clipShape(RoundedRectangle(cornerRadius: 24))
+
+                        Text("Germinated")
+                            .padding()
+                            .glassEffect(.regular.tint(Color(hex:"DFFFE9")).interactive(), in: Capsule())
+                            .frame(maxWidth: .infinity, alignment: .topLeading)
+                            .padding()
+
+                        Text("Tips")
+                            .font(.system(size: 24, weight: .bold, design: .rounded))
+                            .frame(maxWidth: .infinity, alignment: .topLeading)
+                            .padding(.bottom)
+                            .padding(.horizontal)
+
+                        Text("Journal")
+                            .font(.system(size: 24, weight: .bold, design: .rounded))
+                            .frame(maxWidth: .infinity, alignment: .topLeading)
+                            .padding(.bottom)
+                            .padding(.horizontal)
+
+                        NavigationLink(destination: ImageView()) {
+                            VStack {
+                                Image(systemName: "plus")
+                                    .font(.system(size: 28, weight: .medium))
+                                    .padding(.bottom, 6)
+
+                                Text("Add Photo")
+                                    .foregroundColor(.black)
+                                    .font(.system(size: 16))
+                            }
+                            .frame(maxWidth: .infinity, minHeight: 140)
+                            .background(Color(.systemGray5))
+                            .cornerRadius(16)
+                        }
+                        .buttonStyle(.plain)
+                        .padding(.horizontal)
+                    }
+                }
+
                 Spacer()
-                if plantVM.plants[0].plantAge == 1{
-                    Text("\(plantVM.plants[0].plantAge) day old")
-                        .fontWeight(.semibold)
-                }
-                else{
-                    Text("\(plantVM.plants[0].plantAge) days old")
-                        .fontWeight(.semibold)
-                }
-                
             }
             .padding()
-            if selectedDetent == .medium || selectedDetent == .large{
-                VStack{
-                    VStack{
-                        Text("Should germinate in \( plantVM.plants[0].daysToGerminate) days")
-                            .padding(.horizontal)
-                            .padding(.top)
-                            .fontWeight(.medium)
-                        Text(plantVM.plants[0].message)
-                            .padding(.bottom)
-                    }
-                    //.frame(minWidth: 350)
-                    .background(.black.opacity(0.12))
-                    .clipShape(RoundedRectangle(cornerRadius: 24))
-                    
-                    Text("Germinated")
-                        .padding()
-                        .glassEffect(.regular.tint(Color(hex:"DFFFE9")).interactive(),in: Capsule())
-                        .frame(maxWidth: .infinity, alignment: .topLeading)
-                        .padding()
-                        .onTapGesture {
-                            //Next page
-                        }
-                    Text("Tips")
-                        .font(.system(size: 24, weight: .bold, design: .rounded))
-                        .frame(maxWidth: .infinity, alignment: .topLeading)
-                        .padding(.bottom)
-                        .padding(.horizontal)
-                    
-                }
-            }
-            
         }
-        .padding()
-        
-        Spacer()
     }
 }
 
 #Preview{
-    PlantSheet(selectedDetent: .constant(.medium))
+    Plantsheet2(selectedDetent: .constant(.medium))
 }
 
